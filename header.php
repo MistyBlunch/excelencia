724f4a8929a1ec6cdb947f362aed889916c04178
Alex Aragón <aragcar@gmail.com>
2019-08-31 15:35:59 -0500

    menu fix

--- header.php
+++ header.php
@@ -123,7 +123,7 @@
                     ?>
                     <img width="180px" src="<?php  echo $image[0]; ?>"/>
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
 
